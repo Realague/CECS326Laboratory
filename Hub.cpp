1e7742064b1e3eb62fb4a2a77492e2d26595aa88
Schlitzohr <wemx836@gmail.com>
2020-02-19 00:33:32 -0800

    a bit more

--- Hub.cpp
+++ Hub.cpp
@@ -1,3 +1,26 @@
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<iostream>
+#include"Message.h"
+using namespace std;
+
 int main() {
+    int qid = msgget(ftok(".",'u'),IPC_EXCL|IPC_CREAT|0600);
+    Message buf;
+    int size = sizeof(buf) - sizeof(long);
+    msgrcv(qid,(struct Message *)&buf,size, 117, 0);
+    cout << "Hub has recieved message"<<endl;
+    cout << "message: "<< buf.greeting << endl;
+    strcpy(buf.greeting,"Audios");
+    cout << "Hub sends reply" << endl;
+    msgsnd(qid,(struct Message *)&buf,size,0);
+    cout << "Hub shutting down ..." << endl;
     
+    msgrcv(qid,(struct Message *)&buf, size, -112, 0);
+    msgrcv(qid,(struct Message *)&buf,size,0,0);
+
+    msgrcv(qid,(struct Message *)&buf, size, 117, 0);
+    
+    msgctl(qid,IPC_RMID,NULL);
+    exit(0);
 }
---
+++ Message.h
@@ -0,0 +1,7 @@
+#include<string>
+#include<cstring>
+struct Message
+{
+    long mtype;//required
+    char greeting[50];
+};
